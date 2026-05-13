d2240ffda0e25772c8f051781d2699dcddf6a7d7
owengmz <heber12398@gmail.com>
2026-05-13 11:12:11 -0300

    Add CSS styles, robots.txt, and sitemap.xml for site optimization
    - Created a new CSS file for font and layout styles.
    - Added robots.txt to allow all user agents and specify the sitemap location.
    - Introduced sitemap.xml to define the site's structure for search engines.

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,9 @@ export default defineConfig({
   plugins: [
     viteStaticCopy({
       targets: [
-        { src: 'noise.webp', dest: '.' }
+        { src: 'noise.webp', dest: '.' },
+        { src: 'public/sitemap.xml', dest: '.' },
+        { src: 'public/robots.txt', dest: '.' }
       ]
     })
   ]
